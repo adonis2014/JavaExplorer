25bc699fecb9a6ae2cf939cfb6c1d0234e35f09a
peter.chen <peter.chen@FBWXDPeterChen.freeborders.local>
2013-01-22 18:26:10 +0800

    2013-1-22

---
+++ SpringMvc3.1/src/name/chenyuelin/configuration/ServletConfiguration.java
@@ -0,0 +1,51 @@
+/* * @(#)
+		$Id: servletConfiguration.java, peter.chen Exp $
+		*****************************************************************************
+		* Copyright (c) 2013 - Owned by OverStock. All rights reserved.
+		This software is the confidential and proprietary information of
+		OverStock ("Confidential Information"). You shall not disclose such
+		Confidential Information and shall use it only in accordance with
+		the terms of the license agreement you entered into with OverStock.
+		*
+		*****************************************************************************
+		*/
+package name.chenyuelin.configuration;
+
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+/**
+ * @ClassName: servletConfiguration
+ * @Description: TODO(descript the function of this class)
+ * @author peter.chen
+ * @date 2013-1-22 ÏÂÎç4:08:50
+ * 
+ */
+@Configuration
+public class ServletConfiguration {
+    
+    private Environment environment;  
+  
+    @Bean(name="globalObjectMapper")
+    public ObjectMapper createGlobalObjectMapper(){
+        ObjectMapper objectMapper=new ObjectMapper();
+        objectMapper.disable(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES);
+        /*objectMapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.getSerializationConfig().setDateFormat(myDateFormat);
+        final DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        objectMapper.setDateFormat(myDateFormat); // 1.8 and above
+        objectMapper.getSerializationConfig().setDateFormat(myDateFormat); // for earlier versions (deprecated for 1.8+)*/
+        return objectMapper;
+    }
+
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+}
--- SpringMvc3.1/src/name/chenyuelin/constants/BaseConstants.java
+++ SpringMvc3.1/src/name/chenyuelin/constants/BaseConstants.java
@@ -1,23 +1,8 @@
 package name.chenyuelin.constants;
 
-import org.codehaus.jackson.map.DeserializationConfig;
-import org.codehaus.jackson.map.ObjectMapper;
 
 public final class BaseConstants {
 	private BaseConstants() {
 	}
 
-	public static final ObjectMapper DEFAULT_OBJECT_MAPPER = createDefaultObjectMapper();
-
-	private static final ObjectMapper createDefaultObjectMapper() {
-		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.disable(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES);
-		/*objectMapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		objectMapper.configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
-		objectMapper.getSerializationConfig().setDateFormat(myDateFormat);
-		final DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-		objectMapper.setDateFormat(myDateFormat); // 1.8 and above
-		objectMapper.getSerializationConfig().setDateFormat(myDateFormat); // for earlier versions (deprecated for 1.8+)
-*/		return objectMapper;
-	}
 }
