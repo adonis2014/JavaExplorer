6d00240b2367bfb62c31d91fabd94dcaaabc688d
cyl_home <772441445@qq.com>
2013-06-11 17:40:28 +0800

    2013-6-11

---
+++ SpringMvc3.1/src/integrationTest/AllTests.java
@@ -0,0 +1,25 @@
+package integrationTest;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ PersonTest.class })
+public class AllTests {
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println("setUpBeforeClass");
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println("tearDownAfterClass");
+	}
+
+}
