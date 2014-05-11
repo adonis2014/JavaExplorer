e8fcc61f85f2b5ad3be5591482c370d793af2e39
cyl_home <772441445@qq.com>
2014-05-11 22:10:51 +0800

    Add some small function but not complete.

---
+++ SpringMvc3.1/src/name/chenyuelin/ws/endpoint/PersonEndpoint.java
@@ -0,0 +1,26 @@
+package name.chenyuelin.ws.endpoint;
+
+import name.chenyuelin.service.UserService;
+import name.chenyuelin.webapp.dto.PersonDtoListWrap;
+import name.chenyuelin.webapp.transformer.PersonTransformer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Component
+@Endpoint
+public class PersonEndpoint {
+	@Autowired
+	private UserService userService;
+	
+	@PayloadRoot(localPart="getAllPersonsRequest")
+	@ResponsePayload
+	public PersonDtoListWrap getAllPersons() throws Exception{
+		PersonDtoListWrap warp=new PersonDtoListWrap();
+		warp.setPerson(PersonTransformer.toPersonDtoList(userService.getAllPersons()));
+		return warp;
+	}
+}
