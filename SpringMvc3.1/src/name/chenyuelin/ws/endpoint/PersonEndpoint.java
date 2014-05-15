aa7aaf45f27a2d6820443dd13a220762cf7d620e
CYL_HASEE <772441445@qq.com>
2014-05-15 16:42:38 +0800

    Update for some small function.

--- SpringMvc3.1/src/name/chenyuelin/ws/endpoint/PersonEndpoint.java
+++ SpringMvc3.1/src/name/chenyuelin/ws/endpoint/PersonEndpoint.java
@@ -1,5 +1,11 @@
 package name.chenyuelin.ws.endpoint;
 
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXSource;
+
 import name.chenyuelin.entity.test.Person;
 import name.chenyuelin.service.UserService;
 import name.chenyuelin.webapp.dto.PersonDto;
@@ -13,10 +19,14 @@ import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.ws.soap.SoapBody;
+import org.springframework.xml.transform.StringResult;
 
 @Component
 @Endpoint
 public class PersonEndpoint {
+	private static final TransformerFactory DEFAULT_TRANSFORMER_FACTORY=TransformerFactory.newInstance();
+	
 	@Autowired
 	private UserService userService;
 
@@ -32,7 +42,17 @@ public class PersonEndpoint {
 
 	@PayloadRoot(localPart = "findPersonRequest", namespace = "http://www.cyl.org/person/schemas")
 	@ResponsePayload
-	public PersonDto findPerson(@RequestPayload PersonCommand command) {
+	public PersonDto findPerson(@RequestPayload PersonCommand command, @RequestPayload SAXSource saxSource,SoapBody soapBody) throws Exception{
+		Transformer transformer=DEFAULT_TRANSFORMER_FACTORY.newTransformer();
+		transformer.setOutputProperty(OutputKeys.STANDALONE, "yes");
+		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		Result result=new StringResult();
+		transformer.transform(saxSource, result);
+		System.out.println(result.toString());
+		result=new StringResult();
+		transformer.transform(soapBody.getPayloadSource(), result);
+		System.out.println(result.toString());
+		System.out.println(soapBody.getPayloadResult().toString());
 		Person p = userService.findPerson(command.getId());
 		PersonDto person = PersonTransformer.toPersonDto(p);
 		return person;
