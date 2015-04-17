0f475d510523046aa5f26ddf5bc26089b3b8dfff
tpc <tpc@WX-CSC-MS>
2015-04-17 16:48:36 +0800

    添加junit测试sample

---
+++ Junit4Sample/src/test/order/OrderOneTestCase.java
@@ -0,0 +1,40 @@
+package test.order;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class OrderOneTestCase {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("test2");
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println("test1");
+	}
+
+}
